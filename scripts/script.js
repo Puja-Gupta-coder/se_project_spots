0c33ff387c72573e177a9e21af958cdb8e80400c
Puja Gupta <Puja.nathani123@gmail.com>
2025-02-06 00:38:13 -0600

    card Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -33,15 +33,50 @@ const initialCards = [
 const editButton = document.querySelector(".profile__edit-button");
 const editModal = document.querySelector("#edit-modal");
 const closeButton = editModal.querySelector(".modal__button-close");
+const submitButton = editModal.querySelector(".modal__button-submit");
+const modalForm = document.querySelector(".modal__form");
+const profileFormElement = document.querySelector(".profile__column");
+const profileNameElement = profileFormElement.querySelector(".profile__title");
+const profileJobElement = profileFormElement.querySelector(
+  ".profile__description"
+);
+const nameInput = modalForm.querySelector("#name");
+const jobInput = modalForm.querySelector("#description");
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".cards__list");
 
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  return cardElement;
+}
 function handleEditButton() {
   editModal.classList.add("modal_opened");
-  console.log(editModal.classList);
-}
-function handlecloseButton() {
-  editModal.classList.remove("modal_opened");
-  console.log(editModal.classList);
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileJobElement.textContent;
 }
 
+function handleCloseButton() {
+  editModal.classList.remove("modal_opened");
+}
 editButton.addEventListener("click", handleEditButton);
-closeButton.addEventListener("click", handlecloseButton);
+closeButton.addEventListener("click", handleCloseButton);
+
+function handleSubmitButton(evt) {
+  evt.preventDefault();
+  editModal.classList.remove("modal_opened");
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
+}
+
+submitButton.addEventListener("click", handleSubmitButton);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardList.prepend(cardElement);
+}
